bf21486c4d45a2488ac41bf36f4ebb53e9efe52b
dilihe <596228834@qq.com>
2020-01-20 10:49:38 +0800

    更换pass

---
+++ gitproject/src/com/dlh/git/hello/Test03.java
@@ -0,0 +1,13 @@
+package com.dlh.git.hello;
+
+/**
+ * @author helidi
+ * @title: Test03
+ * @projectName FirstIdeaRepo
+ * @date 2020/1/20 10:48
+ */
+public class Test03 {
+    public static void main(String[] args) {
+        System.out.println("666");
+    }
+}
