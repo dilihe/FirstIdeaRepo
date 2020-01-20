25f13465753d086d71ee72658e0636ef575c0533
dilihe <596228834@qq.com>
2020-01-20 11:00:42 +0800

    主分支测试

---
+++ gitproject/src/com/dlh/git/hello/Test04.java
@@ -0,0 +1,13 @@
+package com.dlh.git.hello;
+
+/**
+ * @author helidi
+ * @title: Test04
+ * @projectName FirstIdeaRepo
+ * @date 2020/1/20 10:59
+ */
+public class Test04 {
+    public static void main(String[] args) {
+        System.out.println("主分支");
+    }
+}
