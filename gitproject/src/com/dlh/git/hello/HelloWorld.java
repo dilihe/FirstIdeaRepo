3d91f345e80f62c483f677a0c2691202d53f2dca
dilihe <596228834@qq.com>
2020-01-20 10:40:24 +0800

    提交两个main test文件

---
+++ gitproject/src/com/dlh/git/hello/HelloWorld.java
@@ -0,0 +1,32 @@
+package com.dlh.git.hello;
+
+/**
+ * @author helidi
+ * @title: HelloWorld
+ * @projectName FirstIdeaRepo
+ * @date 2020/1/20 10:22
+ */
+public class HelloWorld {
+
+    /**
+     * main 方法的快捷键是psvm
+     * @param args
+     */
+    public static void main(String[] args) {
+
+//        输出的快捷键是：“输出内容”.sout
+        System.out.println("标准输出的快捷键是sout");
+
+        System.out.println("HelloWorld.main");
+
+        System.out.println("args = [" + args + "]");
+
+        System.out.println("args = " + args);
+
+
+    }
+
+
+
+
+ }
---
+++ gitproject/src/com/dlh/git/hello/Test02.java
@@ -0,0 +1,14 @@
+package com.dlh.git.hello;
+
+/**
+ * @author helidi
+ * @title: Test02
+ * @projectName FirstIdeaRepo
+ * @date 2020/1/20 10:36
+ */
+public class Test02 {
+
+    public static void main(String[] args) {
+        System.out.println("哈哈哈");
+    }
+}
